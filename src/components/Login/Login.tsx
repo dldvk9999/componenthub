47a6174721a56cfb75bf18a1e00369a415c6ee18
JongGeun <dldvk9999@naver.com>
2023-09-10 14:36:36 +0900

    refact: craco 설치 및 SCSS global 파일 import 리팩토링

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,27 +2,56 @@ import { useState } from "react";
 import "./Login.scss";
 
 function Login() {
-    const [id, setId] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
 
     return (
-        <section className="App login">
-            <h2>Login</h2>
-            <input
-                id="id"
-                type="email"
-                placeholder="아이디를 입력해주세요"
-                onChange={(e) => setId(e.target.value)}
-                value={id}
-            />
-            <input
-                id="password"
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <button>로그인</button>
+        <section className="App loginContainer">
+            <div className="login">
+                <h2>Login</h2>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="아이디를 입력해주세요"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+                <button>로그인</button>
+            </div>
+            <div className="singup">
+                <h2>SignUp</h2>
+                <input
+                    id="id"
+                    type="email"
+                    placeholder="아이디를 입력해주세요"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="비밀번호를 다시 입력해주세요"
+                    onChange={(e) => setPassword2(e.target.value)}
+                    value={password2}
+                />
+                <button>회원가입</button>
+            </div>
         </section>
     );
 }
