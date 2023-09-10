813955ac067fc18a70e2c6a0a4e6c038aa9b0bcf
JongGeun <dldvk9999@naver.com>
2023-09-10 15:49:43 +0900

    design: 로그인페이지 디자인 변경

--- src/components/LoginSignup/LoginSignup.tsx
+++ src/components/LoginSignup/LoginSignup.tsx
@@ -2,55 +2,69 @@ import { useState } from "react";
 import "./LoginSignup.scss";
 
 function LoginSignup() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [password2, setPassword2] = useState("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [password2, setPassword2] = useState<string>("");
+    const [nowState, setState] = useState<"Login" | "Signup">("Login");
 
     return (
-        <section className="App loginContainer">
-            <div className="login">
-                <h2>Login</h2>
-                <input
-                    id="email"
-                    type="email"
-                    placeholder="이메일을 입력해주세요"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-                <input
-                    id="password"
-                    type="password"
-                    placeholder="비밀번호를 입력해주세요"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                />
-                <button>로그인</button>
-            </div>
-            <div className="singup">
-                <h2>SignUp</h2>
-                <input
-                    id="id"
-                    type="email"
-                    placeholder="이메일을 입력해주세요"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-                <input
-                    id="password"
-                    type="password"
-                    placeholder="비밀번호를 입력해주세요"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                />
-
-                <input
-                    id="password"
-                    type="password"
-                    placeholder="비밀번호를 다시 입력해주세요"
-                    onChange={(e) => setPassword2(e.target.value)}
-                    value={password2}
-                />
-                <button>회원가입</button>
+        <section className="App LoginSignupContainer">
+            <div className="LoginSignup">
+                <div className="login">
+                    {nowState === "Login" ? (
+                        <>
+                            <h2>Login</h2>
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="이메일을 입력해주세요"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="비밀번호를 입력해주세요"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                            />
+                            <button>로그인</button>
+                        </>
+                    ) : (
+                        <button onClick={() => setState("Login")}>로그인하기</button>
+                    )}
+                </div>
+                <div className="singup">
+                    {nowState === "Signup" ? (
+                        <>
+                            <h2>SignUp</h2>
+                            <input
+                                id="id"
+                                type="email"
+                                placeholder="이메일을 입력해주세요"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="비밀번호를 입력해주세요"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                            />
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="비밀번호를 다시 입력해주세요"
+                                onChange={(e) => setPassword2(e.target.value)}
+                                value={password2}
+                            />
+                            <button>회원가입</button>
+                        </>
+                    ) : (
+                        <button onClick={() => setState("Signup")}>회원가입하기</button>
+                    )}
+                </div>
             </div>
         </section>
     );
