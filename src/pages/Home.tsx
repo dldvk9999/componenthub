7d1d5e3d3edec390ab9c28bca015153a82d5d906
JongGeun <dldvk9999@naver.com>
2023-09-16 20:32:25 +0900

    design: Homa Header 애니메이션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import "../styles/Home.scss";
 
 function Home() {
     return (
-        <section className="App">
-            <header className="App-header">
+        <section className="Home">
+            <header className="Home-header">
                 <h1>ComponentHub</h1>
                 <i>Create, Share, and Use components!</i>
             </header>
