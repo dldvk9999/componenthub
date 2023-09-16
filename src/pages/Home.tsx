268529580ef63b89f7566c27f68dd1884fee5c0a
JongGeun <dldvk9999@naver.com>
2023-09-16 22:30:36 +0900

    design: Home 페이지 컴포넌트 구역 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,66 @@
+import { useCallback, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { v4 as uuidv4 } from "uuid";
 import "../styles/Home.scss";
+import { JSX } from "react/jsx-runtime";
 
 function Home() {
+    const [ref, inView] = useInView();
+    const [cards, setCards] = useState<Array<JSX.Element>>([]);
+    const [isLoading, setLoading] = useState<boolean>(true);
+
+    const cardInit = useCallback(() => {
+        const result: JSX.Element[] = [];
+        for (let i = 0; i < 10; i++) {
+            result.push(
+                <div className="Home-card" key={uuidv4()}>
+                    <img src={process.env.PUBLIC_URL + "/login/signin.jpeg"} alt="test" />
+                    <div className="Home-card-text">
+                        <div className="Home-card-text-title">
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        </div>
+                        <p className="Home-card-text-desc">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit voluptas odit odio
+                            accusamus autem ducimus itaque earum. Quia est aut, facilis fugit saepe blanditiis sequi
+                            exercitationem, maxime necessitatibus ab laudantium.
+                        </p>
+                        <p className="Home-card-text-maker">
+                            code by <i>maker</i>
+                        </p>
+                    </div>
+                </div>
+            );
+        }
+        setCards((card) => [...card, ...result]);
+    }, []);
+
+    // loading이 화면에 보일때는 감지해서 무한 스크롤
+    useEffect(() => {
+        if (!inView) return;
+        else cardInit();
+    }, [cardInit, inView]);
+
+    // 카드가 100개 이상이면 중지
+    useEffect(() => {
+        if (cards.length >= 100) setLoading(false);
+    }, [cards]);
+
     return (
         <section className="Home">
             <header className="Home-header">
                 <h1>ComponentHub</h1>
                 <i>Create, Share, and Use components!</i>
             </header>
+
+            <main>
+                <section className="Home-cards">{cards}</section>
+
+                {isLoading && (
+                    <section ref={ref} className="Home-loading">
+                        Loading ...
+                    </section>
+                )}
+            </main>
 
             <footer className="Home-footer">
                 powered by{" "}
--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -2,6 +2,27 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/Home";
 
+// 가짜 IntersectionObserver 생성
+global.IntersectionObserver = class IntersectionObserver {
+    root!: null;
+    rootMargin!: string;
+    thresholds!: number[];
+    takeRecords!: any;
+    constructor() {}
+
+    observe() {
+        return null;
+    }
+
+    disconnect() {
+        return null;
+    }
+
+    unobserve() {
+        return null;
+    }
+};
+
 it("Home test", () => {
     render(<Home />);
     const linkElement = screen.getByText(/COMPONENTHUB/i);
