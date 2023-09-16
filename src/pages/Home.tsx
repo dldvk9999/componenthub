710847d2b8489733d027dae1c0b1ad5e1fab870c
JongGeun <dldvk9999@naver.com>
2023-09-16 20:40:56 +0900

    design: Home 페이지 footer 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,13 @@ function Home() {
                 <h1>ComponentHub</h1>
                 <i>Create, Share, and Use components!</i>
             </header>
+
+            <footer className="Home-footer">
+                powered by{" "}
+                <a href="https://github.com/dldvk9999" rel="noopener noreferrer">
+                    dldvk9999
+                </a>
+            </footer>
         </section>
     );
 }
