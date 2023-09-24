ee53cc0ed39e9e7b8826e8058e2f5c137d3fc73d
JongGeun <dldvk9999@naver.com>
2023-09-24 21:58:15 +0900

    feat: Navigation init

---
+++ src/components/dldvk9999/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import "./Navigation.scss";
+import MenuList from "./Data/menuList.json";
+
+function Navigation() {
+    return (
+        <section className="Navigation">
+            {Object.entries(MenuList).map((menu, MENU_INDEX) => (
+                <div className="NavigationMenu" key={MENU_INDEX}>
+                    <h2>{menu[0]}</h2>
+                    <div className={`NavigationSub`}>
+                        {Object.entries(menu[1]).map((sub, SUB_INDEX) => (
+                            <a href={sub[1]} key={SUB_INDEX}>
+                                {sub[0]}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </section>
+    );
+}
+
+export default Navigation;
